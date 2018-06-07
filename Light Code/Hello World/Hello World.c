6cc6047cd6586ba7929e06bde14071009f95fef4
blAs1N <hwiasin210@naver.com>
2018-06-07 20:18:31 +0900

    Clear

--- Light Code/Hello World/Hello World.c
+++
@@ -1,7 +0,0 @@
-#include <D:\lib\gcc\mingw32\6.3.0\include\ssp\stdio.h>
-
-int main() {
-	printf("Hello, World!");
-	
-	return 0;
-}
--- Light Code/RSP/RSP/RSP.h
+++
@@ -1,4 +0,0 @@
-#include "C_head.h"
-#pragma warning (disable:4996)
-
-void Process(int user, int com);					// 처리 및 출력 모듈

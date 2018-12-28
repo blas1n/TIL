1090e58cf0bb145fa721500be6901805e8b33d8f
blAs1N <hwiasin210@naver.com>
2018-12-28 14:40:16 +0900

    Solve Fibonacci, Sangsu, Stack

---
+++ Baekjoon Judge/Sangsu/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void PrintReverse(int n) {
+	for (; n > 0; n /= 10)
+		printf("%d", n % 10);
+	printf("\n");
+}
+
+int max(int lhs, int rhs) {
+	return lhs > rhs ? lhs : rhs;
+}
+
+int main() {
+	int a, b, ra = 0, rb = 0;
+	scanf("%d %d", &a, &b);
+
+	for (int i = 100; i > 0; i /= 10) {
+		ra += (a % 10) * i;
+		rb += (b % 10) * i;
+
+		a /= 10;
+		b /= 10;
+	}
+
+	printf("%d\n", max(ra, rb));
+	return 0;
+}

d358f3255f445cbc2580a346020a4a6d660f1ef6
blAs1N <hwiasin210@naver.com>
2019-10-02 23:30:56 +0900

    Make new spring boot project.

--- Spring Boot/Bulletin Board/src/main/java/com/bulletinboard/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bulletinboard.repository;
-
-import com.bulletinboard.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}

66c5877245696a935d63036f65062c82ddecb2fb
blAs1N <qazasa123@gmail.com>
2019-12-19 19:18:13 +0900

    Make my school homework project.

---
+++ Python/Scholarship Analysis/Analysis.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+csv = pd.read_csv('Mertit_Based_Aid.csv')
+print(csv)
+
+csv = pd.read_csv('Need_Based_Aid.csv')
+print(csv)
+
+csv = pd.read_csv('Work_Based_Aid.csv')
+print(csv)

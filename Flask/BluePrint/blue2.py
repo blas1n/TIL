cefd870af3a317c72652b85dbb252c43deb0c09e
blAs1N <qazasa123@gmail.com>
2020-09-18 13:01:45 +0900

    Remove files

--- Flask/BluePrint/blue2.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-blue200 = Blueprint('blue200', __name__)
-
-@blue200.route('/test4')
-def test4() :
-    html = render_template('sub2/test4.html')
-    return html
-
--- Flask/Mini Project/완료(버그 있을 수 있음~)/main/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-main_blue = Blueprint('main_blue', __name__)
-
-@main_blue.route('/')
-def index() :
-    html = render_template('main/index.html')
-    return html

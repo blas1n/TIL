7e2ed46b8ac383bcdb4dc8c7ef444a30df585ee3
blAs1N <qazasa123@gmail.com>
2021-05-19 19:51:04 +0900

    Maintain render manager

--- DirectX/DirectX 11/Source/RenderManager.cpp
+++ DirectX/DirectX 11/Source/RenderManager.cpp
@@ -1,74 +1,39 @@
-#include "GraphicsClass.h"
+#include "RenderManager.h"
+#include "D3DManager.h"
 
-bool GraphicsClass::Init(std::tuple<int, int> screenSize, HWND hWnd) {
-	auto screenWidth = 0, screenHeight = 0;
+constexpr static float SCREEN_FAR = 1000.0f;
+constexpr static float SCREEN_NEAR = 0.1f;
+constexpr static bool VSYNC_ENABLED = true;
+constexpr static bool FULL_SCREEN = true;
 
-	std::tie(screenWidth, screenHeight) = std::move(screenSize);
-
-	m_D3D = std::make_unique<D3dClass>();
-	auto result = m_D3D->Init(screenWidth, screenHeight, VSYNC_ENABLED, hWnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
-	
-	if (!result) {
+bool RenderManager::Initialize(HWND hWnd, POINT size)
+{
+	d3d = new D3DManager{};
+	bool result = d3d->Initialize(hWnd, size, VSYNC_ENABLED, FULL_SCREEN, SCREEN_FAR, SCREEN_NEAR);
+	if (!result)
+	{
 		MessageBox(hWnd, TEXT("Could not initialize Direct3D"), TEXT("Error"), MB_OK);
 		return false;
 	}
 
-	m_camera = std::make_unique<CameraClass>();
-	if (!m_camera.get()) return false;
-
-	m_camera->SetPosition(0.0f, 0.0f, -10.0f);
-
-	m_model = std::make_unique<ModelClass>();
-	if (!m_model.get()) return false;
-
-	result = m_model->Init(m_D3D->GetDevice());
-	if (!result) {
-		MessageBox(hWnd, TEXT("Could not initialize the model object"), TEXT("Error"), MB_OK);
-		return false;
-	}
-
-	m_colorShader = std::make_unique<ColorShaderClass>();
-	if (!m_colorShader.get()) return false;
-
-	result = m_colorShader->Init(m_D3D->GetDevice(), hWnd);
-	if (!result) {
-		MessageBox(hWnd, TEXT("Could not initalize the color shader object"), TEXT("Error"), MB_OK);
-		return false;
-	}
-	
 	return true;
 }
 
-void GraphicsClass::Release() {
-	if (m_colorShader.get())
-		m_colorShader->Release();
+bool RenderManager::Frame()
+{
+	d3d->BeginScene(0.5f, 0.5f, 0.5f, 1.0f);
 
-	if (m_model.get())
-		m_model->Release();
-
-	if (m_D3D.get())
-		m_D3D->Release();
-}
-
-bool GraphicsClass::Frame() {
-	return Render();
-}
-
-bool GraphicsClass::Render() {
-	m_D3D->BeginScene(0.5f, 0.5f, 0.5f, 1.0f);
-
-	m_camera->Render();
-
-	auto viewMatrix = m_camera->GetViewMatrix();
-	auto worldMatrix = m_D3D->GetWorldMatrix();
-	auto projectionMatrix = m_D3D->GetProjectionMatrix();
-	
-	m_model->Render(m_D3D->GetDeviceContext());
-	
-	auto result = m_colorShader->Render(m_D3D->GetDeviceContext(), m_model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix);
-	if (!result) return false;
-
-	m_D3D->EndScene();
+	d3d->EndScene();
 
 	return true;
+}
+
+void RenderManager::Release()
+{
+	if (d3d)
+	{
+		d3d->Release();
+		delete d3d;
+		d3d = nullptr;
+	}
 }
--- DirectX/DirectX 11/Source/RenderManager.h
+++ DirectX/DirectX 11/Source/RenderManager.h
@@ -1,31 +1,14 @@
 #pragma once
 
-#include "D3DManager.h"
-#include <memory>
-#include <tuple>
-#include "ColorShaderClass.h"
-#include "CameraClass.h"
-#include "ModelClass.h"
-
-constexpr static  float SCREEN_DEPTH = 1000.0f;
-constexpr static  float SCREEN_NEAR = 0.1f;
-constexpr static bool VSYNC_ENABLED = true;
-constexpr static bool FULL_SCREEN = true;
+#include "stdafx.h"
 
 class RenderManager final
 {
 public:
-	bool Init(std::tuple<int, int>, HWND);
-	void Release();
-
-	bool Frame();
-
-private:
-	bool Render();
+	[[nodiscard]] bool Initialize(HWND hWnd, POINT size);
+	[[nodiscard]] bool Frame();
+	[[nodiscard]] void Release();
 
 private:
-	std::unique_ptr<D3DManager> m_D3D;
-	std::unique_ptr<CameraClass> m_camera;
-	std::unique_ptr<ModelClass> m_model;
-	std::unique_ptr<ColorShaderClass> m_colorShader;
+	class D3DManager* d3d;
 };

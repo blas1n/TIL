1dad58fbb8ac8d3e88bdd6d010cd2c516c8a81b1
blAs1N <qazasa123@gmail.com>
2021-05-25 00:47:14 +0900

    Use single shader class

--- DirectX/DirectX 11/Source/ColorShader.cpp
+++ DirectX/DirectX 11/Source/ColorShader.cpp
@@ -1,25 +1,168 @@
 #include "ColorShader.h"
 #include <d3d11.h>
+#include <d3dcompiler.h>
+#include <fstream>
 
-std::vector<D3D11_INPUT_ELEMENT_DESC> ColorShader::GetInputElements() const
+#pragma comment(lib, "D3DCompiler.lib")
+
+struct MatrixBufferType final
 {
-	std::vector<D3D11_INPUT_ELEMENT_DESC> elements(2);
+	DirectX::XMMATRIX world;
+	DirectX::XMMATRIX viewProjection;
+};
 
-	elements[0].SemanticName = "POSITION";
-	elements[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
-	elements[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-	elements[0].SemanticIndex = elements[0].InputSlot = elements[0].AlignedByteOffset = elements[0].InstanceDataStepRate = 0;
+bool ColorShader::Initialize(ID3D11Device* device, HWND hWnd)
+{
+	constexpr static auto MissingShader = TEXT("Missing Shader File");
 
-	elements[1].SemanticName = "COLOR";
-	elements[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	elements[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-	elements[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-	elements[1].SemanticIndex = elements[0].InputSlot = elements[0].InstanceDataStepRate = 0;
+	constexpr auto vsName = TEXT("ColorShader/ColorVertexShader.hlsl");
+	constexpr auto psName = TEXT("ColorShader/ColorPixelShader.hlsl");
 
-	return elements;
+	ID3DBlob* errorMessage = nullptr;
+
+	ID3DBlob* vertexShaderBuffer;
+	HRESULT result = D3DCompileFromFile(vsName, nullptr, nullptr,
+		"main", "vs_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 0, &vertexShaderBuffer, &errorMessage);
+
+	if (FAILED(result))
+	{
+		if (errorMessage)
+			OutputShaderError(errorMessage, hWnd, vsName);
+		else
+			MessageBox(hWnd, vsName, MissingShader, MB_OK);
+
+		return false;
+	}
+
+	ID3DBlob* pixelShaderBuffer;
+	result = D3DCompileFromFile(psName, nullptr, nullptr,
+		"main", "ps_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 0, &pixelShaderBuffer, &errorMessage);
+
+	if (FAILED(result))
+	{
+		if (errorMessage)
+			OutputShaderError(errorMessage, hWnd, psName);
+		else
+			MessageBox(hWnd, psName, MissingShader, MB_OK);
+
+		return false;
+	}
+
+	result = device->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), nullptr, &vertexShader);
+	if (FAILED(result)) return false;
+
+	result = device->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(), pixelShaderBuffer->GetBufferSize(), nullptr, &pixelShader);
+	if (FAILED(result)) return false;
+
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[2];
+	polygonLayout[0].SemanticName = "POSITION";
+	polygonLayout[0].SemanticIndex = 0;
+	polygonLayout[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[0].InputSlot = polygonLayout[0].AlignedByteOffset = 0;
+	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[0].InstanceDataStepRate = 0;
+
+	polygonLayout[1].SemanticName = "COLOR";
+	polygonLayout[1].SemanticIndex = 0;
+	polygonLayout[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	polygonLayout[1].InputSlot = 0;
+	polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[1].InstanceDataStepRate = 0;
+
+	const UINT numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
+	result = device->CreateInputLayout(polygonLayout, numElements,
+		vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), &inputLayout);
+
+	if (FAILED(result)) return false;
+
+	vertexShaderBuffer->Release();
+	pixelShaderBuffer->Release();
+
+	D3D11_BUFFER_DESC matrixBufferDesc;
+	matrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	matrixBufferDesc.ByteWidth = sizeof(MatrixBufferType);
+	matrixBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	matrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	matrixBufferDesc.MiscFlags = matrixBufferDesc.StructureByteStride = 0;
+
+	result = device->CreateBuffer(&matrixBufferDesc, nullptr, &matrixBuffer);
+	if (FAILED(result)) return false;
+
+	return true;
 }
 
-std::tuple<const TCHAR*, const TCHAR*> ColorShader::GetShaderNames() const
+bool ColorShader::Render(ID3D11DeviceContext* context, UINT indexCount,
+	DirectX::FXMMATRIX world, DirectX::CXMMATRIX view, DirectX::CXMMATRIX projection)
 {
-	return std::make_tuple(TEXT("Shader/ColorVertexShader.hlsl"), TEXT("Shader/ColorPixelShader.hlsl"));
+	const bool result = SetParameter(context, world, view, projection);
+	if (!result) return false;
+
+	context->IASetInputLayout(inputLayout);
+	context->VSSetShader(vertexShader, nullptr, 0);
+	context->PSSetShader(pixelShader, nullptr, 0);
+	context->DrawIndexed(indexCount, 0, 0);
+	return true;
+}
+
+void ColorShader::Release() noexcept
+{
+	if (matrixBuffer)
+	{
+		matrixBuffer->Release();
+		matrixBuffer = nullptr;
+	}
+
+	if (inputLayout)
+	{
+		inputLayout->Release();
+		inputLayout = nullptr;
+	}
+
+	if (pixelShader)
+	{
+		pixelShader->Release();
+		pixelShader = nullptr;
+	}
+
+	if (vertexShader)
+	{
+		vertexShader->Release();
+		vertexShader = nullptr;
+	}
+}
+
+bool ColorShader::SetParameter(ID3D11DeviceContext* context,
+	DirectX::FXMMATRIX worldMatrix, DirectX::CXMMATRIX viewMatrix, DirectX::CXMMATRIX projectionMatrix)
+{
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	HRESULT result = context->Map(matrixBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	if (FAILED(result)) return false;
+
+	MatrixBufferType* data = reinterpret_cast<MatrixBufferType*>(mappedResource.pData);
+	data->world = DirectX::XMMatrixTranspose(worldMatrix);
+	data->viewProjection = DirectX::XMMatrixTranspose(viewMatrix * projectionMatrix);
+
+	context->Unmap(matrixBuffer, 0);
+
+	UINT bufferNum = 0;
+	context->VSSetConstantBuffers(bufferNum, 1, &matrixBuffer);
+	return true;
+}
+
+void ColorShader::OutputShaderError(ID3D10Blob* errorMsg, HWND hWnd, LPCTSTR shader)
+{
+	constexpr static auto ErrorShader = TEXT("Error compiling shader. Check shader-error.txt for message.");
+
+	char* compileErrors = reinterpret_cast<char*>(errorMsg->GetBufferPointer());
+	const auto bufferSize = errorMsg->GetBufferSize();
+
+	std::ofstream out{ "shader-error.txt" };
+	for (size_t i = 0; i < bufferSize; ++i)
+		out << compileErrors[i];
+
+	out.close();
+
+	errorMsg->Release();
+	MessageBox(hWnd, ErrorShader, shader, MB_OK);
 }
--- DirectX/DirectX 11/Source/ColorShader.h
+++ DirectX/DirectX 11/Source/ColorShader.h
@@ -1,9 +1,26 @@
 #pragma once
 
-#include "Shader.h"
+#include "stdafx.h"
+#include <DirectXMath.h>
 
-class ColorShader final : public Shader
+class ColorShader final
 {
-	std::vector<D3D11_INPUT_ELEMENT_DESC> GetInputElements() const override;
-	std::tuple<const TCHAR*, const TCHAR*> GetShaderNames() const override;
+public:
+	[[nodiscard]] bool Initialize(struct ID3D11Device* device, HWND hWnd);
+	[[nodiscard]] bool Render(struct ID3D11DeviceContext* context, UINT indexCount,
+		DirectX::FXMMATRIX world, DirectX::CXMMATRIX view, DirectX::CXMMATRIX projection);
+
+	void Release() noexcept;
+
+	bool SetParameter(ID3D11DeviceContext* context, DirectX::FXMMATRIX worldMatrix,
+		DirectX::CXMMATRIX viewMatrix, DirectX::CXMMATRIX projectionMatrix);
+
+private:
+	void OutputShaderError(struct ID3D10Blob* errorMsg, HWND hWnd, LPCTSTR shader);
+
+private:
+	struct ID3D11VertexShader* vertexShader = nullptr;
+	struct ID3D11PixelShader* pixelShader = nullptr;
+	struct ID3D11InputLayout* inputLayout = nullptr;
+	struct ID3D11Buffer* matrixBuffer = nullptr;
 };

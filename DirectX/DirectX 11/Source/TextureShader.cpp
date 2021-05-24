dc61a500d0788e6455a133f240683c2dc5a26815
blAs1N <qazasa123@gmail.com>
2021-05-25 00:47:58 +0900

    Fix wrong path

--- DirectX/DirectX 11/Source/TextureShader.cpp
+++ DirectX/DirectX 11/Source/TextureShader.cpp
@@ -1,7 +1,8 @@
-#include "Shader.h"
+#include "TextureShader.h"
 #include <d3d11.h>
 #include <d3dcompiler.h>
 #include <fstream>
+#include "Texture.h"
 
 #pragma comment(lib, "D3DCompiler.lib")
 
@@ -11,14 +12,12 @@ struct MatrixBufferType final
 	DirectX::XMMATRIX viewProjection;
 };
 
-bool Shader::Initialize(ID3D11Device* device, HWND hWnd)
+bool TextureShader::Initialize(ID3D11Device* device, HWND hWnd)
 {
 	constexpr static auto MissingShader = TEXT("Missing Shader File");
 
-	const auto names = GetShaderNames();
-
-	const auto vsName = std::get<0>(names);
-	const auto psName = std::get<1>(names);
+	constexpr auto vsName = TEXT("Shader/TextureVertexShader.hlsl");
+	constexpr auto psName = TEXT("Shader/TexturePixelShader.hlsl");
 
 	ID3DBlob* errorMessage = nullptr;
 
@@ -56,8 +55,24 @@ bool Shader::Initialize(ID3D11Device* device, HWND hWnd)
 	result = device->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(), pixelShaderBuffer->GetBufferSize(), nullptr, &pixelShader);
 	if (FAILED(result)) return false;
 
-	const auto inputElements = GetInputElements();
-	result = device->CreateInputLayout(inputElements.data(), static_cast<UINT>(inputElements.size()),
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[2];
+	polygonLayout[0].SemanticName = "POSITION";
+	polygonLayout[0].SemanticIndex = 0;
+	polygonLayout[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[0].InputSlot = polygonLayout[0].AlignedByteOffset = 0;
+	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[0].InstanceDataStepRate = 0;
+
+	polygonLayout[1].SemanticName = "TEXCOORD";
+	polygonLayout[1].SemanticIndex = 0;
+	polygonLayout[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	polygonLayout[1].InputSlot = 0;
+	polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[1].InstanceDataStepRate = 0;
+
+	const UINT numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
+	result = device->CreateInputLayout(polygonLayout, numElements,
 		vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), &inputLayout);
 
 	if (FAILED(result)) return false;
@@ -75,24 +90,44 @@ bool Shader::Initialize(ID3D11Device* device, HWND hWnd)
 	result = device->CreateBuffer(&matrixBufferDesc, nullptr, &matrixBuffer);
 	if (FAILED(result)) return false;
 
+	D3D11_SAMPLER_DESC samplerDesc;
+	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.AddressU = samplerDesc.AddressV = samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.MipLODBias = 0.0f;
+	samplerDesc.MaxAnisotropy = 1;
+	samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+	samplerDesc.BorderColor[0] = samplerDesc.BorderColor[1] = samplerDesc.BorderColor[2] = samplerDesc.BorderColor[3] = 0;
+	samplerDesc.MinLOD = 0;
+	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+
+	result = device->CreateSamplerState(&samplerDesc, &samplerState);
+	if (FAILED(result)) return false;
+
 	return true;
 }
 
-bool Shader::Render(ID3D11DeviceContext* context, UINT indexCount,
+bool TextureShader::Render(ID3D11DeviceContext* context, UINT indexCount, Texture* texture,
 	DirectX::FXMMATRIX world, DirectX::CXMMATRIX view, DirectX::CXMMATRIX projection)
 {
-	const bool result = SetParameter(context, world, view, projection);
+	const bool result = SetParameter(context, texture, world, view, projection);
 	if (!result) return false;
 
 	context->IASetInputLayout(inputLayout);
 	context->VSSetShader(vertexShader, nullptr, 0);
 	context->PSSetShader(pixelShader, nullptr, 0);
+	context->PSSetSamplers(0, 1, &samplerState);
 	context->DrawIndexed(indexCount, 0, 0);
 	return true;
 }
 
-void Shader::Release() noexcept
+void TextureShader::Release() noexcept
 {
+	if (samplerState)
+	{
+		samplerState->Release();
+		samplerState = 0;
+	}
+
 	if (matrixBuffer)
 	{
 		matrixBuffer->Release();
@@ -118,7 +153,7 @@ void Shader::Release() noexcept
 	}
 }
 
-bool Shader::SetParameter(ID3D11DeviceContext* context,
+bool TextureShader::SetParameter(ID3D11DeviceContext* context, Texture* texture,
 	DirectX::FXMMATRIX worldMatrix, DirectX::CXMMATRIX viewMatrix, DirectX::CXMMATRIX projectionMatrix)
 {
 	D3D11_MAPPED_SUBRESOURCE mappedResource;
@@ -133,10 +168,13 @@ bool Shader::SetParameter(ID3D11DeviceContext* context,
 
 	UINT bufferNum = 0;
 	context->VSSetConstantBuffers(bufferNum, 1, &matrixBuffer);
+
+	const auto resource = texture->GetTexture();
+	context->PSSetShaderResources(0, 1, &resource);
 	return true;
 }
 
-void Shader::OutputShaderError(ID3D10Blob* errorMsg, HWND hWnd, LPCTSTR shader)
+void TextureShader::OutputShaderError(ID3D10Blob* errorMsg, HWND hWnd, LPCTSTR shader)
 {
 	constexpr static auto ErrorShader = TEXT("Error compiling shader. Check shader-error.txt for message.");
 
--- DirectX/DirectX 11/Source/TextureShader.h
+++ DirectX/DirectX 11/Source/TextureShader.h
@@ -2,23 +2,18 @@
 
 #include "stdafx.h"
 #include <DirectXMath.h>
-#include <vector>
 
-class Shader
+class TextureShader final
 {
 public:
 	[[nodiscard]] bool Initialize(struct ID3D11Device* device, HWND hWnd);
 	[[nodiscard]] bool Render(struct ID3D11DeviceContext* context, UINT indexCount,
-		DirectX::FXMMATRIX world, DirectX::CXMMATRIX view, DirectX::CXMMATRIX projection);
+		class Texture* texture, DirectX::FXMMATRIX world, DirectX::CXMMATRIX view, DirectX::CXMMATRIX projection);
 
 	void Release() noexcept;
 
-	bool SetParameter(ID3D11DeviceContext* context, DirectX::FXMMATRIX worldMatrix,
-		DirectX::CXMMATRIX viewMatrix, DirectX::CXMMATRIX projectionMatrix);
-
-protected:
-	virtual std::vector<struct D3D11_INPUT_ELEMENT_DESC> GetInputElements() const = 0;
-	virtual std::tuple<const TCHAR*, const TCHAR*> GetShaderNames() const = 0;
+	bool SetParameter(ID3D11DeviceContext* context, Texture* texture,
+		DirectX::FXMMATRIX worldMatrix,	DirectX::CXMMATRIX viewMatrix, DirectX::CXMMATRIX projectionMatrix);
 
 private:
 	void OutputShaderError(struct ID3D10Blob* errorMsg, HWND hWnd, LPCTSTR shader);
@@ -28,4 +23,5 @@ private:
 	struct ID3D11PixelShader* pixelShader = nullptr;
 	struct ID3D11InputLayout* inputLayout = nullptr;
 	struct ID3D11Buffer* matrixBuffer = nullptr;
+	struct ID3D11SamplerState* samplerState = nullptr;
 };

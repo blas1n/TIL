7a73e970b3a1db74149f57acad24f8458ece98fd
blAs1N <qazasa123@gmail.com>
2021-05-28 20:20:04 +0900

    Add ambient light

--- DirectX/DirectX 11/Source/DirectionalLight.h
+++ DirectX/DirectX 11/Source/DirectionalLight.h
@@ -5,13 +5,16 @@
 struct DirectionalLight final
 {
 public:
+	void SetAmbientColor(const DirectX::XMFLOAT4& inAmbientColor) noexcept { diffuseColor = inAmbientColor; }
 	void SetDiffuseColor(const DirectX::XMFLOAT4& inDiffuseColor) noexcept { diffuseColor = inDiffuseColor; }
 	void SetDirection(const DirectX::XMFLOAT3& inDirection) noexcept { direction = inDirection; }
 
+	const DirectX::XMFLOAT4& GetAmbientColor() const noexcept { return ambientColor; }
 	const DirectX::XMFLOAT4& GetDiffuseColor() const noexcept { return diffuseColor; }
 	const DirectX::XMFLOAT3& GetDirection() const noexcept { return direction; }
 
 private:
+	DirectX::XMFLOAT4 ambientColor;
 	DirectX::XMFLOAT4 diffuseColor;
 	DirectX::XMFLOAT3 direction;
 };

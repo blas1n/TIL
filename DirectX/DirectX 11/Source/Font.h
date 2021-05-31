14bf35c54dfb412b00a65acea261d5d673d5860c
blAs1N <qazasa123@gmail.com>
2021-05-31 23:59:44 +0900

    Create font class

---
+++ DirectX/DirectX 11/Source/Font.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <filesystem>
+
+class Font final
+{
+public:
+	[[nodiscard]] bool Initialize(struct ID3D11Device* device,
+		const std::filesystem::path& fontPath, const std::filesystem::path& texturePath);
+
+	void Release();
+
+	void BuildVertexArray(void* vertices, char* sentence, float drawX, float drawY);
+	class Texture* GetTexture() const noexcept { return texture; }
+
+private:
+	bool LoadFontData(const std::filesystem::path& fontPath);
+	bool LoadTexture(ID3D11Device* device, const std::filesystem::path& texturePath);
+
+private:
+	struct FontType* font;
+	Texture* texture;
+};

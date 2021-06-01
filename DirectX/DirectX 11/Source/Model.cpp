7852a866d4fb0d5aa8a4293579d3bed8a304468a
blAs1N <qazasa123@gmail.com>
2021-06-02 01:37:02 +0900

    Apply object

--- DirectX/DirectX 11/Source/Model.cpp
+++ DirectX/DirectX 11/Source/Model.cpp
@@ -37,10 +37,12 @@ void Model::ReadyToRender(struct ID3D11DeviceContext* context)
 	constexpr UINT stride = sizeof(VertexType);
 	constexpr UINT offset = 0;
 
-	rot.y += DirectX::XM_PI * 0.005f;
-	if (rot.y >= DirectX::XM_2PI)
-		rot.y -= DirectX::XM_2PI;
+	static float rot = 0.0f;
+	rot += DirectX::XM_PI * 0.005f;
+	if (rot >= DirectX::XM_2PI)
+		rot -= DirectX::XM_2PI;
 
+	SetRot({ 0.0f, rot, 0.0f });
 	context->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
 	context->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
 	context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
@@ -72,17 +74,6 @@ void Model::Release() noexcept
 		vertexBuffer->Release();
 		vertexBuffer = nullptr;
 	}
-}
-
-DirectX::XMMATRIX Model::GetWorldMatrix() const
-{
-	const auto rotMat = DirectX::XMMatrixRotationRollPitchYawFromVector(DirectX::XMLoadFloat3(&rot));
-	const auto scaleMat = DirectX::XMMatrixScalingFromVector(DirectX::XMLoadFloat3(&scale));
-
-	const auto scaleRot = DirectX::XMMatrixMultiply(scaleMat, rotMat);
-	const auto posMat = DirectX::XMMatrixTranslationFromVector(DirectX::XMLoadFloat3(&pos));
-
-	return DirectX::XMMatrixMultiply(scaleRot, posMat);
 }
 
 bool Model::InitBuffer(ID3D11Device* device)
--- DirectX/DirectX 11/Source/Model.h
+++ DirectX/DirectX 11/Source/Model.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "stdafx.h"
-#include <DirectXMath.h>
+#include "Object.h"
 #include <filesystem>
 
-class Model final
+class Model final : public Object
 {
 public:
 	[[nodiscard]] bool Initialize(struct ID3D11Device* device,
@@ -12,16 +12,6 @@ public:
 
 	void ReadyToRender(struct ID3D11DeviceContext* context);
 	void Release() noexcept;
-
-	DirectX::XMMATRIX GetWorldMatrix() const;
-
-	void SetPos(const DirectX::XMFLOAT3& inPos) noexcept { pos = inPos; }
-	void SetRot(const DirectX::XMFLOAT3& inRot) noexcept { rot = inRot; }
-	void SetScale(const DirectX::XMFLOAT3& inScale) noexcept { scale = inScale; }
-
-	const DirectX::XMFLOAT3& GetPos() const noexcept { return pos; }
-	const DirectX::XMFLOAT3& GetRot() const noexcept { return rot; }
-	const DirectX::XMFLOAT3& GetScale() const noexcept { return scale; }
 
 	class Texture* GetTexture() const noexcept { return texture; }
 	UINT GetVertexCount() const noexcept { return vertexCount; }
@@ -33,9 +23,6 @@ private:
 	bool LoadTexture(ID3D11Device* device, const std::filesystem::path& texturePath);
 
 private:
-	DirectX::XMFLOAT3 pos, rot;
-	DirectX::XMFLOAT3 scale = { 1.0f, 1.0f, 1.0f };
-
 	struct ID3D11Buffer* vertexBuffer = nullptr;
 	ID3D11Buffer* indexBuffer = nullptr;
 

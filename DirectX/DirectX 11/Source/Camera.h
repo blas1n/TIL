eed2b5943ebcea83c20d8c81599998af9af9b43e
blAs1N <qazasa123@gmail.com>
2021-06-05 23:17:26 +0900

    Override GetWorldMatrix

--- DirectX/DirectX 11/Source/Camera.h
+++ DirectX/DirectX 11/Source/Camera.h
@@ -2,4 +2,27 @@
 
 #include "Object.h"
 
-class Camera : public Object {};
+class Camera : public Object
+{
+public:
+	DirectX::XMMATRIX GetWorldMatrix() const override
+	{
+		const float radians = GetRot().y * 0.0174532925f;
+
+		const DirectX::XMFLOAT3 eye = GetPos();
+		const DirectX::XMFLOAT3 up{ 0.0f, 1.0f, 0.0f };
+		
+		const DirectX::XMFLOAT3 lookAt
+		{
+			sinf(radians) + GetPos().x,
+			GetPos().y,
+			cosf(radians) + GetPos().z
+		};
+
+		return DirectX::XMMatrixLookAtLH(
+			DirectX::XMLoadFloat3(&eye),
+			DirectX::XMLoadFloat3(&lookAt),
+			DirectX::XMLoadFloat3(&up)
+		);
+	}
+};

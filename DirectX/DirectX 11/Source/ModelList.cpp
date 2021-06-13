c22b8b9ca89f455dac8f65b67f2821ecca1482b2
blAs1N <qazasa123@gmail.com>
2021-06-13 16:14:19 +0900

    Change to get random point logic

--- DirectX/DirectX 11/Source/ModelList.cpp
+++ DirectX/DirectX 11/Source/ModelList.cpp
@@ -8,14 +8,15 @@ bool ModelList::Initialize(int modelNum)
 	std::random_device rd;
 	std::mt19937 gen{ rd() };
 
-	std::uniform_real_distribution<float> dis{ 0.0f, 1.0f };
+	std::uniform_real_distribution<float> colorDis{ 0.0f, 1.0f };
+	std::uniform_real_distribution<float> pointDis{ -10.0f, 10.0f };
 
 	for (auto& model : models)
 	{
-		model.color = { dis(gen), dis(gen), dis(gen), 1.0f };
-		model.x = dis(gen) * 10.0f;
-		model.y = dis(gen) * 10.0f;
-		model.z = dis(gen) * 10.0f + 5.0f;
+		model.color = { colorDis(gen), colorDis(gen), colorDis(gen), 1.0f };
+		model.x = pointDis(gen);
+		model.y = pointDis(gen);
+		model.z = colorDis(gen) * 10.0f + 5.0f;
 	}
 
 	return true;

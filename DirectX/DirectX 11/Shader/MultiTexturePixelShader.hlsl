22760238ae6e6dd7097da78a3233d1775e5b871e
blAs1N <qazasa123@gmail.com>
2021-06-20 18:36:46 +0900

    Create multi texture pixel shader

---
+++ DirectX/DirectX 11/Shader/MultiTexturePixelShader.hlsl
@@ -0,0 +1,15 @@
+Texture2D shaderTextures[2];
+SamplerState SampleType;
+
+struct PixelInputType
+{
+	float4 position : SV_POSITION;
+	float2 tex : TEXCOORD0;
+};
+
+float4 main(PixelInputType input) : SV_TARGET
+{
+	float color0 = shaderTextures[0].Sample(SampleType, input.tex);
+	float color1 = shaderTextures[1].Sample(SampleType, input.tex);
+	return saturate(color0 * color1 * 2.0);
+}

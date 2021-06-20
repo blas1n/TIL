d6192e7b28af704cb9dfc3367cf5b430604c09ad
blAs1N <qazasa123@gmail.com>
2021-06-20 21:40:04 +0900

    Fix wrong type

--- DirectX/DirectX 11/Shader/MultiTexturePixelShader.hlsl
+++ DirectX/DirectX 11/Shader/MultiTexturePixelShader.hlsl
@@ -9,7 +9,7 @@ struct PixelInputType
 
 float4 main(PixelInputType input) : SV_TARGET
 {
-	float color0 = shaderTextures[0].Sample(SampleType, input.tex);
-	float color1 = shaderTextures[1].Sample(SampleType, input.tex);
+	float4 color0 = shaderTextures[0].Sample(SampleType, input.tex);
+	float4 color1 = shaderTextures[1].Sample(SampleType, input.tex);
 	return saturate(color0 * color1 * 2.0);
 }

444c2a5d5618a75437266bf6c6c1c3e8ed3d759c
blAs1N <qazasa123@gmail.com>
2021-07-11 23:54:04 +0900

    Create bump shader hlsl

---
+++ DirectX/DirectX 11/Shader/BumpMapPixelShader.hlsl
@@ -0,0 +1,30 @@
+Texture2D shaderTextures[2];
+SamplerState SampleType;
+
+cbuffer LightBuffer
+{
+	float4 diffuseColor;
+	float3 lightDirection;
+};
+
+struct PixelInputType
+{
+	float4 position : SV_POSITION;
+	float2 tex : TEXCOORD0;
+	float3 normal : NORMAL;
+	float3 tangent : TANGENT;
+	float3 binormal : BINORMAL;
+};
+
+float4 main(PixelInputType input) : SV_TARGET
+{
+	float4 textureColor = shaderTextures[0].Sample(SampleType, input.tex);
+	float4 bumpMap = shaderTextures[1].Sample(SampleType, input.tex);
+	bumpMap = (bumpMap * 2.0f) - 1.0f;
+
+	bumpNormal = input.normal + bumpMap.x * input.tangent + bumpMap.y * input.binormal;
+	bumpNormal = normalize(bumpNormal);
+
+	float lightIntensity = saturate(dot(bumpNormal, -lightDirection));
+	return saturate(diffuseColor * lightIntensity) * textureColor;
+}
---
+++ DirectX/DirectX 11/Shader/BumpMapVertexShader.hlsl
@@ -0,0 +1,40 @@
+cbuffer MatrixBuffer
+{
+	matrix worldMatrix;
+	matrix viewProjectionMatrix;
+};
+
+struct VertexInputType
+{
+	float4 position : POSITION;
+	float2 tex : TEXCOORD0;
+	float3 normal : NORMAL;
+	float3 tangent : TANGENT;
+	float3 binormal : BINORMAL;
+};
+
+struct PixelInputType
+{
+	float4 position : SV_POSITION;
+	float2 text : TEXCOORD0;
+	float3 normal : NORMAL;
+	float3 tangent : TANGENT;
+	float3 binormal : BINORMAL;
+};
+
+PixelInputType main(VertexInputType input)
+{
+	PixelInputType output;
+
+	input.position.w = 1.0f;
+	output.position = mul(input.position, worldMatrix);
+	output.position = mul(input.position, viewProjectionMatrix);
+
+	output.tex = input.tex;
+	
+	output.normal = normalize(mul(input.normal, (float3x3)worldMatrix));
+	output.tangent = normalize(mul(input.tangent, (float3x3)worldMatrix));
+	output.binormal = normalize(mul(input.binormal, (float3x3)worldMatrix));
+
+	return output;
+}
